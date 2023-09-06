95127d664b2612bae1f75301b17b9b97a032fd28
EC2 Default User <ec2-user@ip-172-31-6-10.ap-northeast-1.compute.internal>
2023-09-06 08:51:37 +0000

    [Finish]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!
    before_action :ensure_correct_user, only: [:edit, :update, :destroy]
 
   def show
@@ -27,7 +28,6 @@ class BooksController < ApplicationController
   end
 
   def update
-    @book = Book.find(params[:id])
     if @book.update(book_params)
       flash[:notice] = 'You have updated book successfully'
       redirect_to book_path(@book.id)
--- app/models/book.rb
+++ app/models/book.rb
@@ -7,11 +7,7 @@ class Book < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200 }
 
-  def get_profile_image
-    (profile_image.attached?) ? profile_image : 'no_image.jpg'
-  end
-  
   def favorited_by?(user)
-    favorites.exists?(user_id: user.id)
+    favorites.where(user_id: user.id).exists?
   end
 end
